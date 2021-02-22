1b0cf7fc1d07f08bbac0c678ce1f8ab0c4c38974
MrGGTo <56320644+MrGGTo@users.noreply.github.com>
2021-02-23 03:39:40 +0800

    Resolved app login flickering

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import LottieView from "lottie-react-native";
 
-import MainScreen from "./app/screens/MainScreen";
 import { AppNavigator } from "./app/navigations/AppNavigator";
 import LoginScreen from "./app/screens/LoginScreen";
 import AuthContext from "./app/auth/context";
-import auth from "./app/api/auth";
 import authStorage from "./app/auth/storage";
 import { axiosInstance } from "./app/api/config";
-import AppLoading from "expo-app-loading";
 import navigationTheme from "./app/themes/navigationTheme";
 
 export default function App() {
@@ -19,30 +15,43 @@ export default function App() {
 
 	const restoreToken = async () => {
 		const token = await authStorage.getToken();
+		console.log(token);
 		if (token) {
 			fetchUser();
 		} else return;
 	};
 
 	const fetchUser = () =>
-		axiosInstance.get(`/api/users/me`).then(({ data }) => {
-			setUser(data);
-		});
+		axiosInstance
+			.get(`/api/users/me`)
+			.then(({ data }) => {
+				console.log(data);
+				setUser(data);
+				setIsReady(true);
+			})
+			.catch((error) => console.log(error))
+			.finally(() => setIsReady(true));
 
-	if (!isReady)
-		return (
-			<AppLoading
-				startAsync={restoreToken}
-				onError={console.warn}
-				onFinish={() => setIsReady(true)}
-			/>
-		);
+	useEffect(() => {
+		restoreToken();
+	}, []);
 
 	return (
 		<AuthContext.Provider value={{ user, setUser }}>
-			<NavigationContainer theme={navigationTheme}>
-				{user ? <AppNavigator /> : <LoginScreen />}
-			</NavigationContainer>
+			{isReady ? (
+				<NavigationContainer theme={navigationTheme}>
+					{user ? <AppNavigator /> : <LoginScreen />}
+				</NavigationContainer>
+			) : (
+				<LottieView
+					source={require("./assets/loading.json")}
+					style={{
+						flex: 1,
+					}}
+					autoPlay
+					loop={false}
+				/>
+			)}
 		</AuthContext.Provider>
 	);
 }
