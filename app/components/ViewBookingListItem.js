128723e31f565a7a337a5cba0bb23e574ed4c462
MrGGTo <56320644+MrGGTo@users.noreply.github.com>
2021-02-26 19:02:52 +0800

    View Bookings time Sorting

--- app/components/ViewBookingListItem.js
+++ app/components/ViewBookingListItem.js
@@ -3,20 +3,23 @@ import { View, StyleSheet, Text } from "react-native";
 import colors from "../themes/colors";
 import presetStyles, { sizing } from "../themes/presetStyles";
 
-function ViewBookingListItem({ bookingData, date, period }) {
+function ViewBookingListItem({ bookingData, date, period, location }) {
 	return (
 		<View
 			style={[
 				presetStyles.shadow,
+				presetStyles.row,
 				{
 					padding: sizing(3),
 					backgroundColor: colors.backgroundPrimary,
 					borderRadius: sizing(2),
-					marginTop: sizing(2),
 				},
 			]}
 		>
-			<Text style={styles.date}>{date}</Text>
+			<View style={styles.leftSide}>
+				<Text style={styles.date}>{date}</Text>
+				<Text style={styles.location}>{location}</Text>
+			</View>
 			<Text style={styles.period}>{period}</Text>
 		</View>
 	);
@@ -24,6 +27,21 @@ function ViewBookingListItem({ bookingData, date, period }) {
 
 const styles = StyleSheet.create({
 	container: {},
+	leftSide: {
+		flex: 1,
+	},
+	date: {
+		fontSize: sizing(3),
+		color: colors.textSecondary,
+	},
+	location: {
+		fontSize: sizing(4),
+		fontWeight: "500",
+	},
+	period: {
+		fontSize: sizing(4),
+		fontWeight: "500",
+	},
 });
 
 export default ViewBookingListItem;
--- app/screens/ViewBookingsScreen.js
+++ app/screens/ViewBookingsScreen.js
@@ -15,20 +15,68 @@ import Screen from "../components/Screen";
 import ViewBookingListItem from "../components/ViewBookingListItem";
 import colors from "../themes/colors";
 import presetStyles, { sizing } from "../themes/presetStyles";
+import useAuth from "../auth/useAuth";
+
+const TimeSection = ({ data, title }) => (
+	<Animatable.View animation="fadeInUp" style={styles.section}>
+		<Text style={styles.header}>{title}</Text>
+		{data.map((item, index) => (
+			<Animatable.View
+				animation="fadeInUp"
+				delay={index * 100}
+				key={item.id}
+				style={styles.bookingListItem}
+			>
+				<ViewBookingListItem
+					bookingData={item}
+					date={moment(item.start_time).format("LL")}
+					period={`${moment(item.start_time).format(
+						"H:mm"
+					)} - ${moment(item.end_time).format("H:mm")}`}
+					location={item.resource.title_en}
+				/>
+			</Animatable.View>
+		))}
+	</Animatable.View>
+);
 
 function ViewBookingsScreen({ navigation }) {
 	const { user } = useAuth();
-	const [bookings, setBookings] = useState([]);
+	const [isLoading, setLoading] = useState(false);
+	const [upcoming, setUpcoming] = useState([]);
+	const [history, setHistory] = useState([]);
 
 	const fetchUserBookings = () => {
+		setLoading(true);
 		axiosInstance(
-			`/api/users/${user.id}/bookings?start=2021-01-24&end=2021-02-30`
+			`/api/users/${user.id}/bookings?start=2021-02-25&end=2021-02-30`
 		)
-			.then(({ data }) => {
-				setBookings(data);
-				console.log(data);
+			.then(({ data: bookings }) => {
+				// console.log(bookings);
+				const current = moment();
+				var upcomingData = [];
+				var historyData = [];
+				bookings.forEach((booking) => {
+					if (moment(booking.start_time).isAfter(current)) {
+						upcomingData.push(booking);
+					} else {
+						historyData.push(booking);
+					}
+				});
+				upcomingData.sort((a, b) =>
+					moment(a.start_time).isAfter(moment(b.start_time))
+				);
+				historyData.sort((a, b) =>
+					moment(a.start_time).isBefore(moment(b.start_time))
+				);
+				setUpcoming(upcomingData);
+				setHistory(historyData);
+				setLoading(false);
 			})
-			.catch((error) => console.log(error));
+			.catch((error) => {
+				console.log(error);
+				setLoading(false);
+			});
 	};
 
 	useEffect(() => {
@@ -50,24 +98,12 @@ function ViewBookingsScreen({ navigation }) {
 					/>
 				</TouchableOpacity>
 				<Text style={styles.title}>My Bookings</Text>
-				<View>
-					<Text style={styles.header}>Upcoming</Text>
-					{bookings.map((item, index) => (
-						<Animatable.View
-							animation="fadeInUp"
-							delay={index * 100 + 100}
-							key={item.id}
-						>
-							<ViewBookingListItem
-								bookingData={item}
-								date={moment(item.start_time).format("LL")}
-								period={`${moment(item.start_time).format(
-									"H:mm"
-								)} - ${moment(item.end_time).format("H:mm")}`}
-							/>
-						</Animatable.View>
-					))}
-				</View>
+				{!isLoading && (
+					<>
+						<TimeSection data={upcoming} title="Upcoming" />
+						<TimeSection data={history} title="History" />
+					</>
+				)}
 			</ScrollView>
 		</Screen>
 	);
@@ -89,6 +125,12 @@ const styles = StyleSheet.create({
 	header: {
 		fontSize: sizing(3.5),
 		color: colors.textSecondary,
+	},
+	section: {
+		marginBottom: sizing(4),
+	},
+	bookingListItem: {
+		marginTop: sizing(3),
 	},
 });
 
